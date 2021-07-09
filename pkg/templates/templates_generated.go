e77db02f6ff6bdb95f7b83f28ad27d4532969aee
t-chamakawa <t-chamakawa@microsoft.com>
2021-07-09 16:25:49 -0700

    Vendored changes

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -4124,7 +4124,7 @@ write_files:
     {{GetVariableProperty "cloudInitData" "reconcilePrivateHostsService"}}
 {{- end}}
 
-# {{- if IsKrustlet}}
+ {{- if IsKrustlet}}
 - path: /etc/systemd/system/krustlet.service
   permissions: "0644"
   encoding: gzip
